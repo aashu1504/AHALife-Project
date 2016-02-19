a0a2770cc8be61d32d5d1fee17969ec760d9f5b4
John Doe <john@sample.com>
2016-02-19 15:02:10 +0530

    auto comment

--- src/Utility/WebDriverProvider.java
+++ src/Utility/WebDriverProvider.java
@@ -1,9 +1,62 @@
 package Utility;
 
 import java.util.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-class WebDriverProvider{
-    public void run(HashMap<String, Object> params){
+public class WebDriverProvider {
+    
+    public static WebDriver driver = null;
+    static String getBrowserType = null;
+    
+    public void getBrowser(HashMap<String, String> browser)
+    {
+         getBrowserType = browser.get("BROWSER");
+         openBrowser(getBrowserType);
+	}
+    
+    public void openBrowser(String browserType)
+    {
+        if(getBrowserType !=null)
+        {
+            switch (browserType) 
+            {
+			case "Chrome":
+				initializeChromeDriver();
+				break;
+			case "Firefox":
+				initializeFirefoxDriver();
+				break;
+			default:
+				initializeFirefoxDriver();
+			}
+        }
+        else
+        {
+            initializeFirefoxDriver();
+        }
         
     }
+    
+    public void initializeFirefoxDriver() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+    
+    public void initializeChromeDriver() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+    
+    public void closeBrowser(HashMap<String, String> closeBrowser)
+    {
+		 driver.close();
+	}
+    
 }
