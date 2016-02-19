e7e4fc1103c3d068f467f2a4cf1054cf417a48ea
John Doe <john@sample.com>
2016-02-19 15:00:33 +0530

    auto comment

--- src/Utility/GenericClass.java
+++ src/Utility/GenericClass.java
@@ -1,9 +1,83 @@
 package Utility;
 
 import java.util.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import Utility.WebDriverProvider;
+import org.openqa.selenium.Keys;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-class GenericClass{
-    public void run(HashMap<String, Object> params){
+public class GenericClass extends WebDriverProvider{
+    
+    public static List<WebElement> webElementCount = null;
         
+    public void navigateToUrl(HashMap<String, String> navigateUrl) throws InterruptedException
+    {
+        String urlToNavigate = navigateUrl.get("URL");
+        driver.get(urlToNavigate);
+        Thread.sleep(4000L);
     }
+    
+    public String getTextFromAnElement(By elementUniqueID)
+    {
+        return driver.findElement(elementUniqueID).getText();
+    }
+    
+    public String getPageTitle()
+    {
+        return driver.getTitle();
+    }
+    
+    public void enterText(By elementUniqueID,String inputText)
+    {
+        driver.findElement(elementUniqueID).sendKeys(inputText);
+    }
+    
+    public void pressKey(By elementUniqueID,Keys key)
+    {
+        driver.findElement(elementUniqueID).sendKeys(key);
+    }
+    
+    public void buttonClick(By elementUniqueID)
+    {
+        driver.findElement(elementUniqueID).click();
+    }
+    
+    public String getAttributeValue(By elementUniqueID,String attributeType)
+    {
+        return driver.findElement(elementUniqueID).getAttribute(attributeType);
+    } 
+    
+    public boolean isElementExist(By xpath)
+    {
+        webElementCount = driver.findElements(xpath);
+		if(webElementCount.size()>0){
+			return true;
+		}else{
+			return false;
+		}
+    } 
+    
+    public int countOfElements(By xpath)
+    {
+        webElementCount = driver.findElements(xpath);
+        return webElementCount.size();
+    }
+    
+    public List<WebElement> listOfWebElements(By xpath)
+    {
+        return driver.findElements(xpath);
+    }
+    
+    public int getResponseCode(String urlString) throws MalformedURLException, IOException
+    {
+        URL urlToCheck = new URL(urlString); 
+        HttpURLConnection urlConnection = (HttpURLConnection) urlToCheck.openConnection();  
+        urlConnection.setRequestMethod("GET");  
+        urlConnection.connect();  
+        return urlConnection.getResponseCode();  
+  }
 }
