4f4057023ed439a710e77b5dffe769d99a58cc37
John Doe <john@sample.com>
2016-02-22 19:04:56 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
@@ -35,6 +35,9 @@ class BrandsLogin extends GenericClass {
     
     public void verifyUserLoginToBrandPortal(HashMap<String, String> userLogin)
     {   
+        
+        boolean islogo = isElementExist(LOGIN_SITE_LOGO);
+        boolean isadmin = isElementExist(ADMIN_LOGIN_TEXT);
         if(!isElementExist(ADMIN_LOGIN_TEXT) && isElementExist(LOGIN_SITE_LOGO))
         {
             Assert.assertTrue(true);
