95eac00f894dc172734a139252410250e00cd52b
John Doe <john@sample.com>
2016-02-22 17:04:10 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/Login.java
+++ src/AHA LIFE BRANDS PORTAL/Login.java
@@ -2,8 +2,12 @@ package AHALIFEBRANDSPORTAL;
 
 import java.util.*;
 
-class Login{
+class Login extends GenericClass {
     public void run(HashMap<String, Object> params){
+        
+    private final By FB_USER_NAME = By.xpath(".//input[@id='email']");
+    private final By FB_USER_PASSWORD = By.xpath(".//input[@id='pass']");
+    private final By FB_LOGIN_NEW = By.xpath(".//button[@type='submit']");
         
     }
 }
