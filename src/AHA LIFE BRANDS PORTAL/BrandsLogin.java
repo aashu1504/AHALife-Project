37b33a94cfff06dfa1976497f1b3ad4727d2c90b
John Doe <john@sample.com>
2016-02-22 19:22:07 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
@@ -12,7 +12,7 @@ class BrandsLogin extends GenericClass {
     private final By BRAND_LOGIN_CLICK = By.xpath(".//input[@type='submit']");
     private final By ADMIN_LOGIN_TEXT = By.xpath(".//a[@href='/admin']");
     private final By LOGIN_SITE_LOGO = By.xpath(".//div[@class='site-logo']");
-    private final By BRAND_LOGOUT_CLICK = By.xpath(".//div[@class='site-logo']");
+    private final By BRAND_LOGOUT_CLICK = By.xpath(".//a[@href='/logout']");
     private final By BRAND_ADMIN_SIGNOUT_CLICK = By.xpath(".//div[@class='site-logo']"); 
     private final By USER_PROFILE_ICON_CLICK = By.xpath(".//span[@class='glyphicon glyphicon-user']");
         
