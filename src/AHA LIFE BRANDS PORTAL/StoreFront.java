ea5915a6262312ba6ab19261867d9ac0b085e7ad
John Doe <john@sample.com>
2016-02-23 13:56:13 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/StoreFront.java
+++ src/AHA LIFE BRANDS PORTAL/StoreFront.java
@@ -3,7 +3,28 @@ package AHA LIFE BRANDS PORTAL;
 import java.util.*;
 
 class StoreFront{
-    public void run(HashMap<String, Object> params){
-        
+    
+    private final By STOREFRONT_CLICK = By.xpath(".//*[@id='storefrontHeader']/div[2]/a");
+    private final By LOGO_UPLOAD = By.xpath(".//aha-filepicker[@id='model.logoUrl']/div/div[2]/div[1]/div");
+	private final By CHOOSE_UPLOAD_FILE = By.xpath(".//*[@id='fileUploadInput']");
+    
+    public void storeFrontClick(HashMap<String, String> storeFront)
+    {
+           pageToLoad();
+		   buttonClick(STOREFRONT_CLICK);
+    }
+    
+    public void logoUploadClick(HashMap<String, String> logoUpload)
+    {
+        pageToLoad();
+		buttonClick(LOGO_UPLOAD);
+        pageToLoad();
+        driver.switchTo().frame("filepicker_dialog");
+    }
+    
+    public void chooseFileToUpload(HashMap<String, String> chooseFile)
+    {
+        pageToLoad();
+        enterText(CHOOSE_UPLOAD_FILE,"D:\\FileUpload\\Desert.jpg");
     }
 }
