3f6b9c880aac5baab09466cfdfd0195328fdddc9
John Doe <john@sample.com>
2016-02-23 13:10:11 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
@@ -93,7 +93,7 @@ class BrandsLogin extends GenericClass {
         pageToLoad();
 		buttonClick(CHOOSE_UPLOAD_FILE);
         Thread.sleep(4000L);    
-        String uploadPath = "D:\\FileUpload\\fileUploadNew.exe" +" " + "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpeg";
+        String uploadPath = "D:\\FileUpload\\fileUploadNew.exe" + " " + "C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpeg";
         System.out.println(uploadPath);
         Runtime.getRuntime().exec(uploadPath); 
     }
