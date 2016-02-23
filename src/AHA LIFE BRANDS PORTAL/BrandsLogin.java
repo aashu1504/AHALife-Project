af37f49d48191b4b714b7819d9ef68bc8752a523
John Doe <john@sample.com>
2016-02-23 12:47:18 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsLogin.java
@@ -87,7 +87,7 @@ class BrandsLogin extends GenericClass {
         //System.out.println(windows.size());
     }
     
-    public void chooseFileToUpload(HashMap<String, String> logoUpload) throws InterruptedException
+    public void chooseFileToUpload(HashMap<String, String> logoUpload) throws InterruptedException, IOException
     {
            pageToLoad();
 		   buttonClick(CHOOSE_UPLOAD_FILE);
