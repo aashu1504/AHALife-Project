028fad6cc347fd46011a27957a20411bf59fdfb6
John Doe <john@sample.com>
2016-02-23 17:40:46 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/DatabaseConnectivity.java
+++ src/AHA LIFE BRANDS PORTAL/DatabaseConnectivity.java
@@ -19,17 +19,7 @@ class DatabaseConnectivity{
        ResultSet rs= stmt.executeQuery(query);     
        rs.next();
        String actualName = rs.getString("CustomerName");  
-       System.out.println(actualName);
-      // while (rs.next()){
-               //     String id = rs.getString(1);                                        
-                //    String lastName = rs.getString(2);  
-                //    String firstName = rs.getString(3);  
-                //    String address = rs.getString(4);  
-               //     String city = rs.getString(5);  
-               //     String postalCode = rs.getString(6);  
-               //     String country = rs.getString(7);
-                //    System. out.println(id +"  "+lastName+"  "+firstName+"  "+address+"  "+city+ " " + postalCode + " " + country);     
-          //  }          
+       System.out.println(actualName);   
        con.close();  
     }
 }
