8174002c2cd5d9d6212b6ef1ecb54dfd965c4fc6
John Doe <john@sample.com>
2016-02-24 12:15:43 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/DatabaseConnectivity.java
+++ src/AHA LIFE BRANDS PORTAL/DatabaseConnectivity.java
@@ -31,27 +31,4 @@ class DatabaseConnectivity{
        String actualName = rs.getString(columnName);  
        System.out.println(actualName);     
     }
-    
-    
-    
-    
-    public void storeFrontClick(HashMap<String, String> storeFront) throws SQLException, ClassNotFoundException
-    {
-       //Retrieving the data from database created.
-	   String dbUrl = "jdbc:mysql://bp01.qa.ahalife.com:3306/onboarding";                   
-       String username = "onboarding";           
-       String password = "cV3qz2W4a";             
-       //Load My SQL Server jdbc driver        
-       Class.forName("com.mysql.jdbc.Driver");         
-       //Create Connection to DB       
-       Connection con = DriverManager.getConnection(dbUrl,username,password);
-        
-       String query = "SELECT EMAILS FROM admin_query where ID = 1;";
-       Statement stmt = con.createStatement();                    
-       ResultSet rs= stmt.executeQuery(query);     
-       rs.next();
-       String actualName = rs.getString("EMAILS");  
-       System.out.println(actualName);   
-       con.close();  
-    }
 }
