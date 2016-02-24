a44140152f2fde2229e85980317b064f62cda53f
John Doe <john@sample.com>
2016-02-24 16:05:33 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
@@ -16,7 +16,7 @@ class BrandsUserRegistration extends GenericClass{
     private final By INVITE_EMAIL = By.xpath(".//div[@class='ae4 UI UJ']/div/div/table/tbody/tr[1]");
     private final By INVITE_URL = By.xpath(".//div[@class='gs']/div[7]/div/div[2]/div[2]/table/tbody/tr/td/div/a");
     private final By INVITE_EMAIL_ID = By.xpath(".//*[@id='username']");
-    private final By PASSWORD = By.xpath(".//*[@id='password']]");
+    private final By PASSWORD = By.xpath(".//*[@id='password']");
     private final By CONFIRM_PASSWORD = By.xpath(".//*[@id='username']");
     private final By FIRST_NAME = By.xpath(".//*[@id='username']");
     private final By LAST_NAME = By.xpath(".//*[@id='username']");
