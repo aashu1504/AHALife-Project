f532cb7a9724826454fa967d43acf1be7083b93a
John Doe <john@sample.com>
2016-02-24 18:52:54 +0530

    auto comment

---
+++ src/AHA LIFE BRANDS PORTAL/AccountBrandSettings.java
@@ -0,0 +1,20 @@
+package AHALIFEBRANDSPORTAL;
+
+import java.util.*;
+
+class AccountBrandSettings{
+    
+    
+    // Enter COMPANY INFORMATION
+    
+    public void enterDistributorName(HashMap<String, String> distributorName)
+    {
+        
+    }
+    
+    public void enterCompanyWebsite(HashMap<String, String> distributorName)
+    {
+        
+    }
+    
+}
