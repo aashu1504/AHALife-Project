422e3538e5ec483341c66c364b88462247b73736
John Doe <john@sample.com>
2016-02-24 13:39:06 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
@@ -4,7 +4,11 @@ import java.util.*;
 
 class BrandsUserRegistration{
     
+    private final By GMAIL_USER_NAME = By.xpath(".//input[@id='username']");
+    private final By GMAIL_PASSWORD = By.xpath(".//input[@id='password']");
+    
     public void getInviteURLFromGmail(HashMap<String, String> gmailCredentials){
+        
       enterText(GMAIL_USER_NAME,gmailCredentials.get("GmailUser"));
       enterText(GMAIL_PASSWORD,gmailCredentials.get("GmailPassword"));
       
