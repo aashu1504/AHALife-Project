37bcaadbf2d9ae5456f4142eea36d25a96dba9d6
John Doe <john@sample.com>
2016-02-24 18:37:40 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandSettings.java
+++ src/AHA LIFE BRANDS PORTAL/BrandSettings.java
@@ -1,9 +1,11 @@
-package AHA LIFE BRANDS PORTAL;
+package AHALIFEBRANDSPORTAL;
 
 import java.util.*;
 
 class BrandSettings{
-    public void run(HashMap<String, Object> params){
+    
+    public void run(HashMap<String, Object> params)
+    {
         
     }
 }
