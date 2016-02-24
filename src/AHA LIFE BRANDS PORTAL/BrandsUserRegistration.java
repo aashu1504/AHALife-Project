7185d8d5cf1cafdc446f79fb3deae597637516b6
John Doe <john@sample.com>
2016-02-24 13:37:01 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
@@ -3,7 +3,11 @@ package AHA LIFE BRANDS PORTAL;
 import java.util.*;
 
 class BrandsUserRegistration{
-    public void run(HashMap<String, Object> params){
+    
+    public void getInviteURLFromGmail(HashMap<String, String> gmailCredentials){
+        
+      String gmailuserName = gmailCredentials.get("GmailUser");  
+      String gmailPassword = gmailCredentials.get("GmailPassword");  
         
     }
 }
