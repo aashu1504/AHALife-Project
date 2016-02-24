b7596103c8a921f867d4057cdde9c269db296ac2
John Doe <john@sample.com>
2016-02-24 19:22:28 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/AccountBrandSettings.java
+++ src/AHA LIFE BRANDS PORTAL/AccountBrandSettings.java
@@ -4,7 +4,21 @@ import java.util.*;
 
 class AccountBrandSettings{
     
-    .//*[@id='model.legalName']
+    private final By BRAND_USER_NAME = By.xpath(".//*[@id='model.legalName']");
+    private final By BRAND_USER_PASSWORD = By.xpath(".//input[@id='password']");
+    private final By BRAND_LOGIN_CLICK = By.xpath(".//input[@type='submit']");
+    private final By ADMIN_LOGIN_TEXT = By.xpath(".//a[@href='/admin']");
+    private final By LOGIN_SITE_LOGO = By.xpath(".//div[@class='site-logo']");
+    private final By BRAND_LOGOUT_CLICK = By.xpath(".//a[@href='/logout']");
+    private final By BRAND_ADMIN_SIGNOUT_CLICK = By.xpath(".//ul[@class='nav navbar-nav navbar-right']/li[2]/a"); 
+    private final By USER_PROFILE_ICON_CLICK = By.xpath(".//span[@class='glyphicon glyphicon-user']");
+    private final By BRAND_LOGIN_TEXT = By.xpath(".//*[@id='loginForm']/fieldset/div[3]/legend");
+    private final By ADMIN_HOME = By.xpath(".//a[@class='navbar-brand']");
+    private final By BRAND_EMAIL_INVITE = By.xpath(".//*[@id='brandEmail']");
+    private final By INVITE_BRAND = By.xpath(".//*[@id='detailsForm']/button");
+    private final By SUCCESS_MESSAGE = By.xpath(".//*[@id='crudIndex']/div");
+    
+    
     
     // Enter BRAND COMPANY INFORMATION  
     public void enterDistributorName(HashMap<String, String> distributorName)
