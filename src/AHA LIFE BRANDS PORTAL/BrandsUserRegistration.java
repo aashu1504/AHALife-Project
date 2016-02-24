e1cd754a95a3bd7b23944705f134e527f3f4529e
John Doe <john@sample.com>
2016-02-24 13:38:12 +0530

    auto comment

--- src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
+++ src/AHA LIFE BRANDS PORTAL/BrandsUserRegistration.java
@@ -6,8 +6,10 @@ class BrandsUserRegistration{
     
     public void getInviteURLFromGmail(HashMap<String, String> gmailCredentials){
         
-      String gmailuserName = gmailCredentials.get("GmailUser");  
-      String gmailPassword = gmailCredentials.get("GmailPassword");  
-        
+      String gmailuserName = gmailCredentials.get("GmailUser");
+      String gmailPassword = gmailCredentials.get("GmailPassword");
+      enterText(BRAND_USER_NAME,gmailCredentials.get("GmailUser"));
+      enterText(BRAND_USER_NAME,gmailCredentials.get("GmailPassword"));
+      
     }
 }
