aa23bd5da513d78e8d1e8501d10ed892dea5f1f0
John Doe <john@sample.com>
2016-02-26 17:25:19 +0530

    auto comment

--- src/AHA LIFE SITE/SiteJoinAndLogin.java
+++ src/AHA LIFE SITE/SiteJoinAndLogin.java
@@ -4,8 +4,6 @@ import java.util.*;
 import Utility.GenericClass;
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 class JoinAndLogin extends GenericClass{
     
