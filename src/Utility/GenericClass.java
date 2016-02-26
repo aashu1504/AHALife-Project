ae3897c0b93f0d217a98e4521d4dbf22772fa9d6
John Doe <john@sample.com>
2016-02-26 13:00:09 +0530

    auto comment

--- src/Utility/GenericClass.java
+++ src/Utility/GenericClass.java
@@ -105,7 +105,7 @@ public class GenericClass extends WebDriverProvider{
     public void mouseHover(By elementUniqueID)
     {
         Actions builder = new Actions(driver);
-        Action mouseOverAtMyProfile = builder.moveToElement(elementUniqueID).build();
+        Action mouseOverAtMyProfile = builder.moveToElement(driver.findElement(elementUniqueID)).build();
         driver.findElement(elementUniqueID).click();
     }
 }
