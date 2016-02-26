abee7e5e47b90203eb7524ef1e5755fba9ef7a5f
John Doe <john@sample.com>
2016-02-26 11:44:37 +0530

    auto comment

--- src/AHA LIFE SITE/SiteJoinAndLogin.java
+++ src/AHA LIFE SITE/SiteJoinAndLogin.java
@@ -13,32 +13,32 @@ class JoinAndLogin extends GenericClass{
     private final By JOIN_NOW_REGISTER = By.xpath(".//input[@value='Join Now']");
     private final By JOIN_FB_CONNECT = By.xpath(".//*[@id='fbConnectBtn']");
     
-    public void clickJoin(HashMap<String, Object> join)
+    public void clickJoin(HashMap<String, String> join)
     {
         buttonClick(JOIN_AHALIFE_SITE);
     }
     
-    public void createUserName(HashMap<String, Object> joinUserName)
+    public void createUserName(HashMap<String, String> joinUserName)
     {
         enterText(JOIN_CREATE_USERNAME,joinUserName.get("CreateAHALifeSiteUserName"));
     }
     
-    public void createPassword(HashMap<String, Object> joinPassword)
+    public void createPassword(HashMap<String, String> joinPassword)
     {
         enterText(JOIN_CREATE_PASSWORD,joinPassword.get("CreateAHALifeSitePassword"));
     }
     
-    public void clickRegister(HashMap<String, Object> joinRegister)
+    public void clickRegister(HashMap<String, String> joinRegister)
     {
         buttonClick(JOIN_NOW_REGISTER);
     }
     
-    public void clickFBConnect(HashMap<String, Object> joinFBConnect)
+    public void clickFBConnect(HashMap<String, String> joinFBConnect)
     {
         buttonClick(JOIN_FB_CONNECT);
     }
     
-    public void clickSignIn(HashMap<String, Object> signIn)
+    public void clickSignIn(HashMap<String, String> signIn)
     {
         buttonClick(SIGNIN_AHALIFE_SITE);
     }
