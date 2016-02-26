944a1b72cfe6f584d90212ce0c24a7658284606e
John Doe <john@sample.com>
2016-02-26 11:33:28 +0530

    auto comment

---
+++ src/AHA LIFE SITE/SiteJoinAndLogin.java
@@ -0,0 +1,13 @@
+package AHALIFESITE;
+
+import java.util.*;
+
+class JoinAndLogin{
+    
+    
+    private final By JOIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
+    public void run(HashMap<String, Object> params)
+    {
+        
+    }
+}
