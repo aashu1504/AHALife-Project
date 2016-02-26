5fc0e7589149d0bbe7bdd13892ac85895b76b48a
John Doe <john@sample.com>
2016-02-26 11:41:29 +0530

    auto comment

--- src/AHA LIFE SITE/SiteJoinAndLogin.java
+++ src/AHA LIFE SITE/SiteJoinAndLogin.java
@@ -1,8 +1,11 @@
 package AHALIFESITE;
 
 import java.util.*;
+import Utility.GenericClass;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
-class JoinAndLogin{
+class JoinAndLogin extends GenericClass{
     
     private final By SIGNIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
     private final By JOIN_CREATE_USERNAME = By.xpath(".//*[@id='j_username']");
