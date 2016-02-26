d3691b1fdba93072d6c1cab14e2c20b4eacab328
John Doe <john@sample.com>
2016-02-26 11:36:22 +0530

    auto comment

--- src/AHA LIFE SITE/SiteJoinAndLogin.java
+++ src/AHA LIFE SITE/SiteJoinAndLogin.java
@@ -9,7 +9,7 @@ class JoinAndLogin{
     private final By JOIN_CREATE_USERNAME = By.xpath(".//*[@id='j_username']");
     private final By JOIN_CREATEPASSWORD = By.xpath(".//*[@id='j_password']");
     private final By JOIN_NOW_REGISTER = By.xpath(".//input[@value='Join Now']");
-    private final By JOIN_USERNAME = By.xpath(".//*[@id='j_username']");
+    private final By JOIN_FB_CONNECT = By.xpath(".//*[@id='fbConnectBtn']");
     
     public void run(HashMap<String, Object> params)
     {
