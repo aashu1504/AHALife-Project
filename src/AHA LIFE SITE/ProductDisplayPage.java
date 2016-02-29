11f41281ac74df6f3de08eb084c0afd75f04d5da
aashu1504 <aashu1504@gmail.com>
2016-02-29 16:30:52 +0530

    auto comment

--- src/AHA LIFE SITE/ProductDisplayPage.java
+++ src/AHA LIFE SITE/ProductDisplayPage.java
@@ -8,16 +8,16 @@ class ProductDisplayPage{
     private final By PRODUCT_NAME = By.xpath(".//div[@class='product_mainContainer']/div/h1");
     
     static String productNameAtProductDisplayPage = null;
+     
+    public void verifyNavigationToProductDisplayPage(HashMap<String, String> productName)
+    {
+        productNameAtProductDisplayPage = getTextFromAnElement(PRODUCT_NAME);
+        Assert.assertTrue(productNameAtProductDisplayPage.equalsIgnoreCase(productName.get("ProductNameAtHomePage")));
+    }
     
     public void clickAddToBagPDP(HashMap<String, String> addPDP)
     {
         wait.until(ExpectedConditions.visibilityOfElementLocated(ADD_TO_BAG_PDP));
         buttonClick(ADD_TO_BAG);
-    }
-    
-    public void verifyNavigationToProductDisplayPage(HashMap<String, String> productName)
-    {
-        productNameAtProductDisplayPage = getTextFromAnElement(PRODUCT_NAME);
-        Assert.assertTrue(productNameAtProductDisplayPage.equalsIgnoreCase(productName.get("ProductNameAtHomePage")));
     }
 }
