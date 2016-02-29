ed3e0e9e986c5db4a47ec594e5c1482ef1c2ca07
aashu1504 <aashu1504@gmail.com>
2016-02-29 16:22:41 +0530

    auto comment

--- src/AHA LIFE SITE/ProductDisplayPage.java
+++ src/AHA LIFE SITE/ProductDisplayPage.java
@@ -5,7 +5,7 @@ import java.util.*;
 class ProductDisplayPage{
     
     private final By ADD_TO_BAG_PDP = By.xpath(".//*[@id='buy-button']");
-    private final By PRODUCT_NAME = By.xpath(".//*[@id='buy-button']");
+    private final By PRODUCT_NAME = By.xpath(".//div[@class='product_mainContainer']/div/h1");
     
     static String productNameAtProductDisplayPage = null;
     
