25aac831e8493025a71179e87c4cd974d2455a6b
aashu1504 <aashu1504@gmail.com>
2016-02-29 16:19:02 +0530

    auto comment

--- src/AHA LIFE SITE/ProductDisplayPage.java
+++ src/AHA LIFE SITE/ProductDisplayPage.java
@@ -3,6 +3,9 @@ package AHALIFESITE;
 import java.util.*;
 
 class ProductDisplayPage{
+    
+    private final By ADD_TO_BAG_PDP = By.xpath(".//*[@id='buy-button']");
+    
     public void run(HashMap<String, Object> params){
         
     }
