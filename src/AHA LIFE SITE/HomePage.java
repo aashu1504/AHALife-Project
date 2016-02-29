9aaa8187c111a03ddf4fe966db465e9eed137c25
aashu1504 <aashu1504@gmail.com>
2016-02-29 18:08:18 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -42,7 +42,7 @@ class HomePage extends GenericClass{
         buttonClick(CLOSE_QUICK_VIEW);
     }
     
-    public void verifySameProductQuickViewOpened(HashMap<String, String> productQuickView)
+    public void verifyProductQuickViewOpened(HashMap<String, String> productQuickView)
     {
         pageToLoad();
         productNameAtQuickView = getTextFromAnElement(PRODUCT_NAME_QUICK_VIEW);
