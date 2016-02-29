818864e559ddabddf101156186eca7e168d4dc9a
aashu1504 <aashu1504@gmail.com>
2016-02-29 15:56:18 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -4,5 +4,6 @@ import java.util.*;
 
 class HomePage{
     
-    
+    private final By PRODUCT_ID = By.xpath(".//li[@class='loginActions']/div/a[1]");
+    private final By JOIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
 }
