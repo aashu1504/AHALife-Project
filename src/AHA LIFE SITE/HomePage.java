3de6c679dfd834c2f89feefb1fc2452061164452
aashu1504 <aashu1504@gmail.com>
2016-02-29 15:56:40 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -4,6 +4,6 @@ import java.util.*;
 
 class HomePage{
     
-    private final By PRODUCT_ID = By.xpath(".//li[@class='loginActions']/div/a[1]");
+    private final By PRODUCT_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]");
     private final By JOIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
 }
