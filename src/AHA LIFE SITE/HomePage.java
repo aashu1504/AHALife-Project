4b748b1f1c6b067d6e70fdb9bd7ed5e7360adf24
aashu1504 <aashu1504@gmail.com>
2016-02-29 16:02:34 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -1,8 +1,11 @@
 package AHALIFESITE;
 
 import java.util.*;
+import Utility.GenericClass;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
-class HomePage{
+class HomePage extends GenericClass{
     
     private final By PRODUCT_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]");
     private final By QUICK_VIEW = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]/a/div[1]/div[3]");
