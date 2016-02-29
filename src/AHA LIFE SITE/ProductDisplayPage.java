0e365f32739c61d9969a35a1e1a79830f5888815
aashu1504 <aashu1504@gmail.com>
2016-02-29 16:34:39 +0530

    auto comment

--- src/AHA LIFE SITE/ProductDisplayPage.java
+++ src/AHA LIFE SITE/ProductDisplayPage.java
@@ -3,8 +3,9 @@ package AHALIFESITE;
 import java.util.*;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import Utility.GenericClass;
 
-class ProductDisplayPage extends GenericClasss{
+class ProductDisplayPage extends GenericClass{
     
     private final By ADD_TO_BAG_PDP = By.xpath(".//*[@id='buy-button']");
     private final By PRODUCT_NAME = By.xpath(".//div[@class='product_mainContainer']/div/h1");
