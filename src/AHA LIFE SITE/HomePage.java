c7af1bc7bd3063286bc0b728cd90decb604ad5e1
aashu1504 <aashu1504@gmail.com>
2016-02-29 12:08:48 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -3,7 +3,9 @@ package AHA LIFE SITE;
 import java.util.*;
 
 class HomePage{
-    public void run(HashMap<String, Object> params){
+    
+    public void run(HashMap<String, String> params)
+    {
         
     }
 }
