70c5a91d34613ade372b5a9daffbdb130931a0ea
John Doe <john@sample.com>
2016-03-01 13:26:00 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayement.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayement.java
@@ -7,6 +7,18 @@ import org.testng.Assert;
 
 class SecureCheckoutAndPayement extends GenericClass{
     
+    private final By FIRST_NAME = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]/a/div[2]/h4");
+    private final By LAST_NAME = By.xpath(".//*[@id='lastName']");
+    private final By ADDRESS_LINE_1 = By.xpath(".//*[@id='buy-button']");
+    private final By CITY = By.xpath(".//*[@id='id-close-quick-view']");
+    private final By ZIP = By.xpath(".//*[@id='id-product-quick-view']/div[3]/div[2]/a[1]");
+    private final By PHONE = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]/a/div[2]/h4");
+    private final By EMAIL_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]/a/div[1]/div[3]");
+    private final By STATE = By.xpath(".//*[@id='buy-button']");
+    private final By COUNTRY = By.xpath(".//*[@id='id-close-quick-view']");
+    private final By PROVINCE = By.xpath(".//*[@id='id-product-quick-view']/div[3]/div[2]/a[1]");
+    private final By CONTINUE = By.xpath(".//*[@id='id-product-quick-view']/div[3]/div[2]/a[1]");
+    
     public void run(HashMap<String, Object> params)
     {
         
