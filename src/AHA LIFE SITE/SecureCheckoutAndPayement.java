c9a008669cbf99344e4e89ed9a552203fbefc82c
John Doe <john@sample.com>
2016-03-01 13:27:01 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayement.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayement.java
@@ -13,7 +13,7 @@ class SecureCheckoutAndPayement extends GenericClass{
     private final By CITY = By.xpath(".//*[@id='city']");
     private final By ZIP = By.xpath(".//*[@id='address.postalCode']");
     private final By PHONE = By.xpath(".//*[@id='phone']");
-    private final By EMAIL_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[1]/a/div[1]/div[3]");
+    private final By EMAIL_ID = By.xpath(".//*[@id='emailAddress']");
     private final By STATE = By.xpath(".//*[@id='buy-button']");
     private final By COUNTRY = By.xpath(".//*[@id='id-close-quick-view']");
     private final By PROVINCE = By.xpath(".//*[@id='id-product-quick-view']/div[3]/div[2]/a[1]");
