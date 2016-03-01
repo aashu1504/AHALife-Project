650f80fa8be71f8b684f06670322a36dd81e8fa1
John Doe <john@sample.com>
2016-03-01 13:19:30 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayement.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayement.java
@@ -3,7 +3,9 @@ package AHA LIFE SITE;
 import java.util.*;
 
 class SecureCheckoutAndPayement{
-    public void run(HashMap<String, Object> params){
+    
+    public void run(HashMap<String, Object> params)
+    {
         
     }
 }
