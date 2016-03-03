7208852115dfd2d9ce968b775c4985e6a8eb0a97
John Doe <john@sample.com>
2016-03-03 14:52:40 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayment.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayment.java
@@ -123,7 +123,7 @@ class SecureCheckoutAndPayment extends GenericClass{
       
     public void verifyPromoApplied(HashMap<String, String> applyPromoCode)
     {
-        int promoCode = getTextFromAnElement(PROMO_DISCOUNT_VALUE).substring(1, 4);
+        String promoCode = getTextFromAnElement(PROMO_DISCOUNT_VALUE).substring(1, 4);
         System.out.println(promoCode);
         //promoCodeValue = Integer.parseInt(getTextFromAnElement(PROMO_DISCOUNT_VALUE).trim().substring(1, 4));
         //System.out.println(promoCodeValue);
