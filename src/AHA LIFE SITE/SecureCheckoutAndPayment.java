488b1fe021b18121bc4e4ffa8549b22ccc3f2fa5
John Doe <john@sample.com>
2016-03-15 15:47:24 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayment.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayment.java
@@ -170,7 +170,7 @@ class SecureCheckoutAndPayment extends GenericClass{
     
     public String getOrderID(HashMap<String, String> order)
     {
-        String orderID = getTextFromAnElement(PLACED_PRODUCT_ORDER_NUMBER);
+        orderID = getTextFromAnElement(PLACED_PRODUCT_ORDER_NUMBER);
         long orderNumber = Long.parseLong(orderID.trim());
         System.out.println("Order ID of the product " + placedProductName + " is " + orderNumber);     
         return orderID;
