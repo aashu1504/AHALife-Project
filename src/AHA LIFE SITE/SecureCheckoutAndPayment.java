4a269684dd7070e7bb4524d067e912c1a508f180
John Doe <john@sample.com>
2016-03-15 15:44:14 +0530

    auto comment

--- src/AHA LIFE SITE/SecureCheckoutAndPayment.java
+++ src/AHA LIFE SITE/SecureCheckoutAndPayment.java
@@ -214,8 +214,6 @@ class SecureCheckoutAndPayment extends GenericClass{
     public void verifyIfOrderedProductIsInUserOrderHistory(HashMap<String, String> verifyOrderHistory)
     {
         placedProductName = getTextFromAnElement(PLACED_PRODUCT_NAME);
-        System.out.println("Product Name At Display Page : " + verifyOrder.get("ProductNameAtDisplayPage"));
-        System.out.println("Placed Product Name : " + getTextFromAnElement(PLACED_PRODUCT_NAME));
         Assert.assertTrue(placedProductName.equalsIgnoreCase(verifyOrder.get("ProductNameAtDisplayPage")));
     }
 }
