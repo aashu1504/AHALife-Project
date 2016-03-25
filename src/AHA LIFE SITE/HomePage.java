87bd7fc6a38a514cb03cf03e38e1f17c95926990
John Doe <john@sample.com>
2016-03-25 16:18:45 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -107,6 +107,15 @@ class HomePage extends GenericClass{
        }
     }
     
+    
+    public void createStopwatch(HashMap<String, String> pageLoadTime)
+    {
+        Stopwatch pageLoad = Stopwatch.createStarted();
+		navigateToUrl();
+		pageLoad.stop();
+	    long pageLoadTime_seconds = pageLoad.elapsed(TimeUnit.SECONDS);
+	    System.out.println("Total Page Load Time: " + pageLoadTime_ms + " Seconds");
+    }
     public void getPageLoadTime(HashMap<String, String> pageLoadTime)
     {
         Stopwatch pageLoad = Stopwatch.createStarted();
