4a200ff27de6b11d339e4ae3c1e740cdeec823f6
John Doe <john@sample.com>
2016-03-25 16:20:03 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -26,7 +26,7 @@ class HomePage extends GenericClass{
     static List<WebElement> urlListElements = null;
     static int statusCode;
     static List<Integer> statusCodeList = new ArrayList<Integer>();
-    Stopwatch pageLoad = null;
+    static Stopwatch pageLoad = null;
 
     public String clickProduct(HashMap<String, String> product)
     {
