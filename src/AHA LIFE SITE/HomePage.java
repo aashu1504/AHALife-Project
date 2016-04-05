30e9a430ea8fbf011b22c16e420c0696a2efdce2
John Doe <john@sample.com>
2016-04-05 16:56:59 +0530

    auto comment

--- src/AHA LIFE SITE/HomePage.java
+++ src/AHA LIFE SITE/HomePage.java
@@ -14,8 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 class HomePage extends GenericClass{
     
-    private final By PRODUCT_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[2]/a/div[2]/h4");
-    private final By QUICK_VIEW = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[2]/a/div[1]/div[3]");
+    private final By PRODUCT_ID = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[3]/a/div[2]/h4");
+    private final By QUICK_VIEW = By.xpath(".//div[@class='container homepage ng-scope']/div[5]/div/div/div/div/ul/li[3]/a/div[1]/div[3]");
     private final By ADD_TO_BAG_HOME = By.xpath(".//*[@id='buy-button']");
     private final By CLOSE_QUICK_VIEW = By.xpath(".//*[@id='id-close-quick-view']");
     private final By PRODUCT_NAME_QUICK_VIEW = By.xpath(".//*[@id='id-product-quick-view']/div[3]/div[2]/a[1]");
