7ec3df99cb546cc378de96b147dd73cf8fd5b2a7
John Doe <john@sample.com>
2016-06-08 16:04:02 +0530

    auto comment

--- src/AHA LIFE SITE/ProductDisplayPageAndCart.java
+++ src/AHA LIFE SITE/ProductDisplayPageAndCart.java
@@ -9,7 +9,7 @@ class ProductDisplayPageAndCart extends GenericClass{
     
     private final By ADD_TO_BAG_PDP = By.xpath(".//*[@id='buy-button']");
     private final By PRODUCT_NAME = By.xpath(".//div[@class='product_mainContainer']/div/h1");
-    private final By MY_BAG = By.xpath(".//*[@id='id-header-top-level-menu']/li[5]/a");
+    private final By MY_BAG = By.xpath(".//*[@id='id-header-top-level-menu']/li[4]/a");
     private final By MY_BAG_LOGGED_IN = By.xpath(".//*[@id='id-header-top-level-menu']/li[4]/a");
     private final By CART_PRODUCT_NAME = By.xpath(".//*[@id='cart_products']/li/div[2]/h3");
     private final By BEGIN_CHECKOUT = By.xpath(".//*[@id='begin-checkout-button']/div/input");
