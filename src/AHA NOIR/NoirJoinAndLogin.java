74088dc0e3738c4828fa5e8881361b9d78de52de
John Doe <john@sample.com>
2016-06-09 14:36:57 +0530

    auto comment

--- src/AHA NOIR/NoirJoinAndLogin.java
+++ src/AHA NOIR/NoirJoinAndLogin.java
@@ -1,6 +1,9 @@
 package AHA NOIR;
 
 import java.util.*;
+import Utility.GenericClass;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
 class NoirJoinAndLogin extends GenericClass{
     
