4f76039a15763237bca5f56d7be57a1324dfba86
John Doe <john@sample.com>
2016-06-09 14:38:53 +0530

    auto comment

--- src/AHA NOIR/NoirJoinAndLogin.java
+++ src/AHA NOIR/NoirJoinAndLogin.java
@@ -7,7 +7,9 @@ import org.testng.Assert;
 
 class NoirJoinAndLogin extends GenericClass{
     
-    private final By JOIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
+    private final By JOIN_AHANOIR_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
+    private final By JOIN_CREATE_USERNAME = By.xpath(".//*[@id='resetInputid']");
+    
     
     public void clickProfile(HashMap<String, String> profile)
     {
