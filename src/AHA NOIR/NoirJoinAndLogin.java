a708401fa5e29b697f08bdc204af9d09e9411340
John Doe <john@sample.com>
2016-06-09 14:31:43 +0530

    auto comment

--- src/AHA NOIR/NoirJoinAndLogin.java
+++ src/AHA NOIR/NoirJoinAndLogin.java
@@ -2,8 +2,15 @@ package AHA NOIR;
 
 import java.util.*;
 
-class NoirJoinAndLogin{
-    public void run(HashMap<String, Object> params){
-        
+class NoirJoinAndLogin extends GenericClass{
+    
+    private final By JOIN_AHALIFE_SITE = By.xpath(".//li[@class='loginActions']/div/a[2]");
+    
+    public void clickProfile(HashMap<String, String> profile)
+    {
+        buttonClick(JOIN_AHALIFE_SITE);
     }
+    
+    
+    
 }
