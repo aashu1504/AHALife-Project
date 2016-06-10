c37f621c1136c03e693062bc2d0360a8ff8dcaee
John Doe <john@sample.com>
2016-06-10 11:27:22 +0530

    auto comment

--- src/AHA NOIR/NoirJoinAndLogin.java
+++ src/AHA NOIR/NoirJoinAndLogin.java
@@ -53,7 +53,7 @@ class NoirJoinAndLogin extends GenericClass{
     {
         mouseHoverOn(MY_PROFILE);
         WebDriverWait wait = new WebDriverWait(driver, 15);
-       wait.until(ExpectedConditions.visibilityOfElementLocated(ACCOUNT_SETTING));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ACCOUNT_SETTING));
     }
     
     public void clickAccountSetting(HashMap<String, String> accountSetting)
